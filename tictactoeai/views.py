1554dc31b0a519375ac2ddaae83f72b123abd30e
dahelo <dohernandez93@gmail.com>
2023-12-04 15:22:21 -0700

    add vercel config

--- tictactoeai/views.py
+++ tictactoeai/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from tictactoegame.views import tictactoeGameView
-
+from django.http import HttpResponse
 
 def index(request):
-    return redirect(tictactoeGameView)
+    return HttpResponse("Hello from index main")
